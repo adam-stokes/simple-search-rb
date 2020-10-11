1839f621e9276f34837dee5e1f97f6a9e47c2d7d
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2020-10-11 14:50:48 -0400

    add simple tf-idf
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

--- eg/main.rb
+++ eg/main.rb
@@ -15,5 +15,15 @@ puts "## OUTPUT"
 Pry::ColorPrinter.pp(matches.records)
 
 
+puts
 puts "## JSON"
 puts JSON.pretty_generate(matches.records.collect { |i| { :id => i.id, :original_text => i.original_text, :text => i.text, :terms => i.terms, :frequency => i.term_frequency}})
+
+
+puts
+puts "## OUTPUT BY RANK"
+
+matches = store.search_by_rank(query)
+matches.each do |match|
+  puts "ID: #{match[0].id}, Text: #{match[0].text}, Score: #{match[1]}"
+end
--- lib/simple/search/store.rb
+++ lib/simple/search/store.rb
@@ -1,3 +1,6 @@
+require 'matrix'
+require 'tf-idf-similarity'
+
 module Simple
   module Search
     class Store
@@ -19,6 +22,20 @@ module Simple
           end
         end
         matches
+      end
+
+      def search_by_rank(term)
+        matches = search(term)
+        records = matches.records.map {|r| r}
+
+        # Create a set of tfidf documents
+        documents = records.map {|record| TfIdfSimilarity::Document.new(record.text)}
+
+        # Create tfidf model
+        model = TfIdfSimilarity::TfIdfModel.new(documents)
+        scores = documents.map {|document| model.tfidf(document, term)}
+        scores = records.zip(scores)
+        scores.sort_by{|k| -k[1]}
       end
     end
   end
